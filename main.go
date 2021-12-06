f527c7634c9c85ed556bebdda50ea2f8bd907307
Adam Bernstein <abernstein@debian-BULLSEYE-live-builder-AMD64>
2021-12-05 18:05:17 -0800

    Added maximum call limit
    Rate limit incoming calls to web server. Generally a good idea,
    although the server processing time is very fast.

--- main.go
+++ main.go
@@ -8,34 +8,42 @@ import (
 	"time"
 )
 
-const shortForm = "2006,01,02,15,04,05"
+const (
+	shortTimeFormat = "2006,01,02,15,04,05"
+	maxClients      = 10
+	httpPort        = ":8081"
+)
 
 func startRestTzServer() {
+	httpMax := make(chan struct{}, maxClients)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		httpMax <- struct{}{}
+		defer func() { <-httpMax }()
+
 		if timeStr, err := timeForTz(html.EscapeString(r.URL.Path)[1:]); err != nil {
 			fmt.Fprintf(w, "No such time zone %q", html.EscapeString(r.URL.Path))
 		} else {
 			fmt.Fprintf(w, "%s", timeStr)
 		}
 	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(httpPort, nil))
 }
 
 func timeForTz(tz string) (string, error) {
-	loc, err := time.LoadLocation(tz)
+	locationTz, err := time.LoadLocation(tz)
 	if err != nil {
 		return "", err
 	}
 
-	n := time.Now().In(loc)
-	nstr := fmt.Sprintf("%4d,%02d,%02d,%02d,%02d,%02d",
+	n := time.Now().In(locationTz)
+	nowTime := fmt.Sprintf("%4d,%02d,%02d,%02d,%02d,%02d",
 		n.Year(), n.Month(), n.Day(), n.Hour(), n.Minute(), n.Second())
-	if t, err := time.ParseInLocation(shortForm, nstr, loc); err != nil {
+	if tzTime, err := time.ParseInLocation(shortTimeFormat, nowTime, locationTz); err != nil {
 		fmt.Println(fmt.Errorf("%v", err))
 		return "", err
 	} else {
-		timeFmt := t.Format(shortForm)
+		timeFmt := tzTime.Format(shortTimeFormat)
 		return timeFmt, nil
 	}
 }
