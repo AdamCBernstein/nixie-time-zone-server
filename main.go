3b56cc419d555a0f81ccd5c74c6df0f7b4778d33
Adam Bernstein <adam.bernstein@gigamon.com>
2021-12-08 09:47:54 -0800

    Add PORT environment variable
    Added PORT environment variable
    Added project documentation

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
+// Go implementation of time-zone-server project.
+// Configure the "time server" for an Ian Sparkes Nixie tube clock to
+// a time zone entry, similar to the following:
+//   http://mpgedit.org:3000/GetTime/Europe/Zurich
+//
+// This will return the time for Zurich Switzerland.
+// Specify your local time zone to keep your clock time automatically set.
+//
+// https://github.com/isparkes/time-zone-server#time-zone-server
+// This server responds to a REST call in this format:
+//   http://host-location:port/GetTime/Country/City
+//
+// Actual example:
+//   http://mpgedit.org:3000/GetTime/America/Los_Angeles
+//
+// Response to this call:
+// 2021,12,08,18,32,18
+// YEAR MM DD HH MM SS
+//
+// These are the supported REST calls:
+//	GetTime
+//	GetTimeRaw
+//	GetTimeOffset
+//	GetTimeZone
+//
+// Build this project:
+//    go build
+//
+// Run this time server:
+//    ./nixie_timeserver &
+//
+// The default port this server is 3000.
+// This can be changed by doing the following:
+//    export PORT=4444
+//   ./nixie_timeserver &
+
 import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -15,11 +52,11 @@ const (
 	restGetTimeZone   = "/GetTimeZone/"
 	shortTimeFormat   = "2006,01,02,15,04,05"
 	maxClients        = 10
-	httpPort          = ":3000"
 )
 
 var (
-	httpMax = make(chan struct{}, maxClients)
+	httpMax  = make(chan struct{}, maxClients)
+	httpPort = ":3000"
 )
 
 func getTime(w http.ResponseWriter, r *http.Request) {
@@ -107,6 +144,10 @@ func getTimeZone(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if len(port) > 0 {
+		httpPort = ":" + port
+	}
 	http.HandleFunc(restGetTime, getTime)
 	http.HandleFunc(restGetTimeRaw, getTimeRaw)
 	http.HandleFunc(restGetTimeOffset, getTimeOffset)
